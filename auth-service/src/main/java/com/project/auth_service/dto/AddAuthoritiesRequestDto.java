d07d36de45279401fb224698c68ad0edaca5cf01
Safa Gith <202201364@bethlehem.edu>
2025-11-24 21:18:06 +0200

    Edit RoleDto

--- auth-service/src/main/java/com/project/auth_service/dto/AddAuthoritiesRequestDto.java
+++ auth-service/src/main/java/com/project/auth_service/dto/AddAuthoritiesRequestDto.java
@@ -2,6 +2,8 @@ package com.project.auth_service.dto;
 
 import lombok.*;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 import java.util.List;
 
 @Data
@@ -10,5 +12,9 @@ import java.util.List;
 @Builder
 public class AddAuthoritiesRequestDto {
     @NotEmpty(message = "Authorities list cannot be empty")
+    @Pattern(
+        regexp = "^[A-Za-z]+$",
+        message = "Role name must contain letters only and no numbers"
+    )
     private List<String> authorities;
 }
--- auth-service/src/main/java/com/project/auth_service/dto/AddRoleDto.java
+++ auth-service/src/main/java/com/project/auth_service/dto/AddRoleDto.java
@@ -1,12 +1,17 @@
 package com.project.auth_service.dto;
 
-
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class AddRoleDto {
+    
     @NotBlank(message = "Role name cannot be blank")
+    @Pattern(
+        regexp = "^[A-Za-z]+$",
+        message = "Role name must contain letters only and no numbers"
+    )
     private String name;
 }
