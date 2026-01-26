e37d27ebe2686f9b25880ded8e7b1b29d004c34a
Safa Gith <202201364@bethlehem.edu>
2026-01-26 20:11:17 +0200

    Edit

--- backend/config-server/src/main/java/com/project/config_server/ConfigServiceApplication.java
+++ backend/config-server/src/main/java/com/project/config_server/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.project.config_service;
+package com.project.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/discovery-server/src/main/java/com/project/discovery_server/DiscoveryServiceApplication.java
+++ backend/discovery-server/src/main/java/com/project/discovery_server/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.project.discovery_service;
+package com.project.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
