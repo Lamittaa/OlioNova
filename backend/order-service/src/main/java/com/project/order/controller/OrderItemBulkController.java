10e758daf1a67a2dd527c7b6ed6197832f079790
Safa Gith <202201364@bethlehem.edu>
2026-03-26 08:51:23 +0200

    Create Order BulkController

---
+++ backend/order-service/src/main/java/com/project/order/controller/OrderItemBulkController.java
@@ -0,0 +1,28 @@
+package com.project.order.controller;
+
+import com.project.order.dto.OrderItemResponse;
+import com.project.order.service.OrderItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders/items") // 🔥 بدون orderId
+@RequiredArgsConstructor
+public class OrderItemBulkController {
+
+    private final OrderItemService orderItemService;
+     
+
+    @PostMapping("/bulk")
+    @PreAuthorize("hasAuthority('ORDER_ITEM_READ')")
+    public ResponseEntity<List<OrderItemResponse>> getItemsByIds(
+            @RequestBody List<Long> itemIds) {
+
+        return ResponseEntity.ok(
+                orderItemService.getItemsByIds(itemIds)
+        );
+    }
+}
